4396c6dc41429eb0d247bd69a03f0495b09bc6ab
wikwanggy <01077428827w@gmail.com>
2022-11-16 14:11:15 +0900

    step 2 인사이트의 발견 : 탐색과 시각화 하기
    내용 간단하게 요점 정리
    
    value_count() 함수 적용
    위 함수는 오로지 이러한 시리즈객체에만 적용
    
    groupby() 함수 적용
    데이터 프레임에서 특정 피처를 기준으로 그룹을 생성하며 이를 통해
    그룹별 연산을 적용할 수 있음
    
    sum(), count()
    count() : 아이템별 주문개수를 의미
    sum() 아이템별 주문 총량을 의미
    
    시각화
    tolist()와 넘파이의 arange() 함수를 이용해 x_pos를 선언하고, 0부터 50까지의 숫자를 그래프의 x축 이름으로
    사용합니다. 그 이유는 50ro 아이템의 이름을 x 축에 표현하기에는 그래프의 공간이 너무 협소하기때문입니다.
    y값(order_id)에는 주문 총량에 해당하는 값인 item_quantity.values,tolist()를 넣어 줍니다.
    
    step 3 데이터 전처리 : 전처리 함수 사용하기
    
    item_price의 수치적 특징을 탐색하는 과정에서 item-price 피처의 요약 통계를 구할 수 없었다.
    이는 item_price 피처가 문자열 타입이었기 때문입니다.
    가격을 나타내는 숫자 앞에 "$"가 있는것을 확인
    따라서 이 피처는 수치 데이터로 사용하기 위해서는 "$"를 제거해주는 전처리 작업이 필요하다.
    "$" 기호를 제거하는 전처리 방법은 chipo['item_price']에 apply() 함수를 적용함으로써 가능합니다
    그리고 apply() 함수에는 lambda 함수 명령어를 추가 해줍니다
    
    apply() : 시리즈 단위의 연산을 처리하는 기능을 수행하고 sum(), mean()과
    같이 연신이 정의된 함수를 파라미터로 받는다.
    
    sum().mean()을 groupby()와 함께 쓰면 한 주문에 10달러 이상 지불한 주문을 찾을 수 있다.

--- python-data-analysis-master/chipotle.py
+++ python-data-analysis-master/chipotle.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 file_path="python-data-analysis-master/data/chipotle.tsv"
 #read.csv() 함수로 데이터를 데이터 프레임 형태(엑셀)로 불러옵니다.
 chipo=pd.read_csv(file_path, sep="\t")
@@ -22,3 +23,45 @@ print(chipo.describe())
 
 print(len(chipo['order_id'].unique())) # order_id의 개수를 출력합니다.
 print(len(chipo['item_name'].unique())) # item_name의 개수를 출력합니다.
+
+# 가장 많이 주문한 아이템 Top 10 출력하기
+item_count=chipo['item_name'].value_counts()[:10]
+for idx, (val, cnt) in enumerate(item_count.iteritems(), 1):
+    print("TOP", idx, ":", val,cnt)
+    
+#아이템별 주문 개수와 총량구하기
+order_count=chipo.groupby('item_name')['order_id'].count()
+order_count[:10] # 아이템별 주문 개수를 출력합니다
+item_quantity=chipo.groupby('item_name')['quantity'].sum()
+item_quantity[:10] #아이템별 주문 총량을 출력합니다
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+item_name_list = item_quantity.index.tolist()
+x_pos=np.arange(len(item_name_list))
+order_cnt=item_quantity.values.tolist()
+
+plt.bar(x_pos, order_cnt, align="center")
+plt.ylabel("ordered_item_count")
+plt.title('Distribution of all orderd item')
+
+plt.show()
+
+# 데이터 전처리: 함수 사용하기
+print(chipo)
+print("-----------")
+chipo["item_price"].head()
+
+#colum 단위 데이터에 apply() 함수로 전처리를 적용합니다.
+chipo['item_price']=chipo['item_price'].apply(lambda x: float(x[1:]))
+print(chipo.describe())
+
+# 주문당 평균 계산금액 출력하기
+chipo.groupby('order_id')['item_price'].sum().mean()
+
+# 한 주문에 10달러 이상 지불한 주문번호(id) 출력하기
+chipo_orderid_group=chipo.groupby("order_id").sum()
+results=chipo_orderid_group[chipo_orderid_group.item_price >= 10]
+print(results[:10])
+print(results.index.values)
