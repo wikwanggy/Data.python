05ccd5d3e65d81b15e7424867f0e5b16b66f0db1
wikwanggy <01077428827w@gmail.com>
2022-11-16 10:47:04 +0900

    데이터에서 인사이트 발견하기 챕터 1 1. 데이터셋의 기초 정보 출력하기. 2. 데이터셋의 행과 열, 데이터 확인하기 3. describe()함수로 기초 통계량 출력하기 4. unique() 함수로 범주형 피처의 개수 출력하기

--- python-data-analysis-master/chipotle.py
+++ python-data-analysis-master/chipotle.py
@@ -8,8 +8,17 @@ print(chipo.shape)
 print("--------------------------------")
 print(chipo.info())
 
+# chipo라는 데이터 프레임에서 순서대로 10개의 데이터를 보여줍니다.
 chipo.head(10)
 
 print(chipo.columns)
 print("--------------------------------")
 print(chipo.index)
+
+#order_id는 숫자의 의미를 가지지 않기 때문에 str으로 변환합니다.
+chipo['order_id']=chipo['order_id'].astype(str)
+# chipo 데이터 프레임 에서 수치형 피처들의 기초 통께량을 확인합니다
+print(chipo.describe()) 
+
+print(len(chipo['order_id'].unique())) # order_id의 개수를 출력합니다.
+print(len(chipo['item_name'].unique())) # item_name의 개수를 출력합니다.
