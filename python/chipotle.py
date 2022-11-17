c147f3905b531d82bcc6599f14a3107567cba79f
wikwanggy <01077428827w@gmail>
2022-11-17 12:16:08 +0900

    국가가 별 음주 데이터 분석하기 - [<Step1. 탐색> 데이터의 기초 정보 살펴보기]     - [drinks 데이터셋의 기본 정보] - [<Step2. 인사이트의 발견> : 탐색과 시각화]     - [피처간의 상관관계 탐색]         - [두 피처간의 상관계수 구하기]         - [여러 피처의 상관관계 분석]     - [가장 많이 주문한 item]     - [주문된 item의 개수]     - [item당 주문 개수와 총량] - [<Step3. 탐색적 분석> : 스무고개로 분석하는 개념적 탐색]     - [결측 데이터 전처리]     - [그룹 단위의 데이터 분석 : 대륙별 분석]     - [그룹 단위의 데이터 분석 : 의미를 가지는 피처 재구성하기] - [<Step4. 통계적 분석> : 분석 대상간의 통계적 차이 검정하기]     - [아프리카와 유럽간의 술 소비량 차이 검정하기] - [대한민국은 얼마나 술을 독하게 마시는 나라일까?]-나라일까?

--- python/chipotle.py
+++ python/chipotle.py
@@ -65,3 +65,49 @@ chipo_orderid_group=chipo.groupby("order_id").sum()
 results=chipo_orderid_group[chipo_orderid_group.item_price >= 10]
 print(results[:10])
 print(results.index.values)
+
+# 각 아이템 가격
+# 동일 아이템을 1개만 구매한 주문
+chipo_one_item=chipo[chipo.quantity==1]
+# 아이템이름별 최저가를 계산
+price_per_item=chipo_one_item.groupby("item_name").min()
+# 가격을 기준으로 정렬(ascending=False(내림차순) ascending=True(오름차순))
+print(price_per_item.sort_values("item_price",ascending=False)[:10])
+
+# 아이템 가격 분포 차트
+item_name_list=price_per_item.index.tolist()
+x_pos=np.arange(len(item_name_list)) # x 축
+item_price=price_per_item["item_price"].tolist() #y 축
+
+plt.bar(x_pos,item_price,align="center")
+plt.ylabel("item price($)")
+plt.title("Distribution of item price")
+plt.show()
+
+# 아이템 가격 히스토그램
+
+plt.hist(item_price)
+plt.ylabel("counts")
+plt.title("Histogram of item price")
+plt.show()
+
+# 가장 비싼 주문에서 아이템이 총 몇개 팔렸는지 구하기
+# chipo.groupby('order_id').sum().sort_values(by='item_price', ascending=False)[:5]
+
+# "Veggie Salad Bowl"이 몇 번 주문되었는지 구하기
+# "Veggie Sanlad Bowl"이 몇번 주문되었는지 계산합니다.
+chipo_salad=chipo[chipo["item_name"]=="Veggie Salad Bowl"]
+# 한 주문 내에서 중복 집계된 item_name을 제거합니다.
+chipo_salad=chipo_salad.drop_duplicates(['item_name',"order_id"])
+
+print(len(chipo_salad))
+chipo_salad.head(5)
+
+# 'Chicken Bowl'을 2개 이상 주문한 주문 횟수 구하기
+# Chicken Bowl 메뉴의 총 주문 수량 구함
+chipo_chicken=chipo[chipo['item_name']=="Chiken Bowl"]
+chipo_chicken_ordersum=chipo_chicken.groupby('order_id').sum()['quantity']
+chipo_chicken_result=chipo_chicken_ordersum[chipo_chicken_ordersum>=2]
+
+print(len(chipo_chicken_result))
+print(chipo_chicken_result.head(5))
---
+++ python/drink.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_path="python-data-analysis-master/data/drinks.csv"
+drinks=pd.read_csv(file_path) 
+print(drinks.info())
+
+# drinks.head(10)
+# drinks.describe()
+
+# # 두 피처 간의 상관 계수 구하기
+# # beer_servings, wine_servings 두 피처 간의 상관 계수를 계산합니다.
+# # pearson은 상관 계수를 구하는 계산 방법 중 하나를 의미하며, 가장 널리 쓰이는 방법
+# corr=drinks[['beer_servings','wine_servings']].corr(method="pearson")
+# print(corr)
+
+# # 여러 피처의 상관 관계 분석하기
+# #
+# cols=['beer_servings','spirit_servings','wine_servings','total_litres_of_pure_alcohol']
+# corr=drinks[cols].corr(method="pearson")
+# print(corr)
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# corr 행렬 히트맵을 시각화 합니다.
+
+# 그래프 출력을 위한 cols 이름을 축약합니다.
+# cols_view=['beer','spirit','wine','alcohol'] 
+# sns.set(font_scale=1.5)
+# hm = sns.heatmap(corr.values,
+#     cbar= True, # 사이드바(cbar)
+#     annot=True, # 네모안에 숫자 표시 
+#     square=True, # 정사각형으로 출력
+#     fmt='.2f',
+#     annot_kws={'size':15},
+#     yticklabels=cols_view,
+#     xticklabels=cols_view)
+
+# plt.tight_layout() 
+# plt.show()
+
+# 시각화 라이브러리를 이용한 피처간의 산점도 그래프를 출력합니다
+# sns.set(style='whitegrid',context='notebook')
+# sns.pairplot(drinks[['beer_servings','spirit_servings','wine_servings','total_litres_of_pure_alcohol']],height=2.5)
+
+# plt.show()
+# null값인지 있는 값인지 확인하기.
+print(drinks['continent'].head(10).isnull())
+# 결측데이터를 처리합니다 : 정보가 없는 국가를 'Others', 줄여서 통합 -> 'OT'
+drinks['continent'] = drinks['continent'].fillna('OT')
+print(drinks.head(10))
+
+
+# 파이차트로 시각화 하기
+labels = drinks['continent'].value_counts().index.tolist()
+fracs1 = drinks['continent'].value_counts().values.tolist()
+explode = (0, 0, 0, 0.25, 0, 0)
+
+plt.pie(fracs1, explode=explode, labels=labels, autopct='%.0f%%', shadow=True)
+plt.title('null data to \'OT\'')
+plt.show()
